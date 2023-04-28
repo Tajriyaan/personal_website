9093c0ad2136bdbc6466c39e30796ec7354fa9cb
Tajriyaan <45387742+Tajriyaan@users.noreply.github.com>
2023-04-28 23:53:51 +0600

    Added  database, add single image, some prompt

--- TajriyaanSiteFinal/config.py
+++
@@ -1,2 +0,0 @@
-mail_username = 'tajriyaanchowdhury97@gmail.com'
-mail_password = 'ihyjknynplhvpvfs'
---
+++ tajriyaansitefinal(latest)/config.py
@@ -0,0 +1,4 @@
+mail_username = 'tajriyaanchowdhury97@gmail.com'
+mail_password = 'ihyjknynplhvpvfs'
+upload_folder = 'static/images/blog/'
+secret_key = "@!asTaksq**$#@"

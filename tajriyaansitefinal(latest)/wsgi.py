7b69cc4ebd256cca55c77196ac79211de9502cd4
tajriyaan <tajriyaanchowdhury97.com>
2023-04-28 22:24:57 +0200

    Added wsgi.py file

---
+++ tajriyaansitefinal(latest)/wsgi.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+# Add the directory containing your app to Python's path
+app_dir = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, app_dir)
+
+# Import your Flask app object
+from app import app as application
